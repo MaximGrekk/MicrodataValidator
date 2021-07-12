7aec5d4de09b39a5cb223eebf656ae6ed13645c6
Shadowharm <pmv.maks@gmail.com>
2021-07-12 17:52:53 +0300

    Add API

---
+++ src/Components/Result/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Result() {
+  return (
+    <div class="alert alert-primary" role="alert">
+        Это основное уведомление — check it out!
+    </div>
+  );
+}
+
+export default Result;
